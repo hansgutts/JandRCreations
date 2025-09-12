6fec93fb71105254f43e895f1fa04d9d9928cff8
Hans Guttormsen <hansgutts@gmail.com>
2025-09-12 00:08:46 -0500

    cleaned up unnecessary fields. keeping commented test values for now

--- JandRCreations/schema.sql
+++ JandRCreations/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE prod_design (
 
 CREATE TABLE prod_type (
     prod_type_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    prod_type TEXT NOT NULL,
+    prod_type TEXT UNIQUE NOT NULL,
     prod_type_description TEXT NOT NULL,
     prod_type_image TEXT NOT NULL,
     prod_design_id TEXT NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE prod_type (
 CREATE TABLE prod (
     prod_id INTEGER PRIMARY KEY AUTOINCREMENT,
     prod_type_id INTEGER,
-    prod_name TEXT NOT NULL,
+    prod_name TEXT UNIQUE NOT NULL,
     prod_description TEXT NOT NULL,
     prod_price REAL NOT NULL,
     prod_cost REAL NOT NULL,
@@ -36,7 +36,6 @@ CREATE TABLE custom (
     prod_id INTEGER NOT NULL,
     custom TEXT NOT NULL,
     custom_desc TEXT NOT NULL,
-    exclusive BOOLEAN NOT NULL,
     require BOOLEAN NOT NULL,
     FOREIGN KEY (prod_id) REFERENCES prod (prod_id)
 );
@@ -69,14 +68,14 @@ INSERT INTO prod_type (prod_type, prod_design_id, prod_type_description, prod_ty
 INSERT INTO prod_type (prod_type, prod_design_id, prod_type_description, prod_type_image) VALUES ('other', 2, 'other things dont fall into a clear category', 'other.png');
 
 INSERT INTO prod (prod_type_id, prod_name, prod_description, prod_price, prod_cost, prod_sold, prod_image) VALUES ('1', 'Hol_Wreath', 'A holiday wreath', 5, 5, False, 'Hol_Wreath.png');
-INSERT INTO prod (prod_type_id, prod_name, prod_description, prod_price, prod_cost, prod_sold, prod_image) VALUES ('2', 'Hol_Wreath', 'A holiday wreath', 5, 5, False, 'Hol_Wreath.png');
-INSERT INTO prod (prod_type_id, prod_name, prod_description, prod_price, prod_cost, prod_sold, prod_image) VALUES ('3', 'Hol_Wreath', 'A holiday wreath', 5, 5, False, 'Hol_Wreath.png');
-INSERT INTO prod (prod_type_id, prod_name, prod_description, prod_price, prod_cost, prod_sold, prod_image) VALUES ('4', 'Hol_Wreath', 'A holiday wreath', 5, 5, False, 'Hol_Wreath.png');
-INSERT INTO prod (prod_type_id, prod_name, prod_description, prod_price, prod_cost, prod_sold, prod_image) VALUES ('5', 'Hol_Wreath', 'A holiday wreath', 5, 5, False, 'Hol_Wreath.png');
-INSERT INTO prod (prod_type_id, prod_name, prod_description, prod_price, prod_cost, prod_sold, prod_image) VALUES ('6', 'Hol_Wreath', 'A holiday wreath', 5, 5, False, 'Hol_Wreath.png');
+INSERT INTO prod (prod_type_id, prod_name, prod_description, prod_price, prod_cost, prod_sold, prod_image) VALUES ('2', 'Hol_Wreath2', 'A holiday wreath', 5, 5, False, 'Hol_Wreath.png');
+INSERT INTO prod (prod_type_id, prod_name, prod_description, prod_price, prod_cost, prod_sold, prod_image) VALUES ('3', 'Hol_Wreath3', 'A holiday wreath', 5, 5, False, 'Hol_Wreath.png');
+INSERT INTO prod (prod_type_id, prod_name, prod_description, prod_price, prod_cost, prod_sold, prod_image) VALUES ('4', 'Hol_Wreath4', 'A holiday wreath', 5, 5, False, 'Hol_Wreath.png');
+INSERT INTO prod (prod_type_id, prod_name, prod_description, prod_price, prod_cost, prod_sold, prod_image) VALUES ('5', 'Hol_Wreath5', 'A holiday wreath', 5, 5, False, 'Hol_Wreath.png');
+INSERT INTO prod (prod_type_id, prod_name, prod_description, prod_price, prod_cost, prod_sold, prod_image) VALUES ('6', 'Hol_Wreath6', 'A holiday wreath', 5, 5, False, 'Hol_Wreath.png');
 
-INSERT INTO custom (custom_id, prod_id, custom, custom_desc, exclusive, require) VALUES (1, 1, 'Color', 'Choose your color', True, False);
-INSERT INTO custom (custom_id, prod_id, custom, custom_desc, exclusive, require) VALUES (2, 1, 'Color', 'Choose your color', True, False);
+INSERT INTO custom (custom_id, prod_id, custom, custom_desc, require) VALUES (1, 1, 'Color1', 'Choose your color', False);
+INSERT INTO custom (custom_id, prod_id, custom, custom_desc, require) VALUES (2, 1, 'Color2', 'Choose your color', False);
 
 INSERT INTO options (options_id, custom_id, cost_change, option_name) VALUES (1, 1, 4.50, 'Blue');
 INSERT INTO options (options_id, custom_id, cost_change, option_name) VALUES (2, 1, 4.50, 'Red');
@@ -87,4 +86,4 @@ INSERT INTO options (options_id, custom_id, cost_change, option_name) VALUES (5,
 INSERT INTO options (options_id, custom_id, cost_change, option_name) VALUES (6, 2, 2.50, 'Red');
 INSERT INTO options (options_id, custom_id, cost_change, option_name) VALUES (7, 2, 2.50, 'orange');
 INSERT INTO options (options_id, custom_id, cost_change, option_name) VALUES (8, 2, 2.50, 'green');
-/*
+*/
