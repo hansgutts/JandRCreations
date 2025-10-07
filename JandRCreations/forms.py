7f6ff7f5de15e490e4cbc53645d8e61f4b25e722
Hans Guttormsen <hansgutts@gmail.com>
2025-10-06 21:02:25 -0500

    added an add/delete form option

--- JandRCreations/forms.py
+++ JandRCreations/forms.py
@@ -3,24 +3,15 @@ from flask import session
 import sqlite3
 
 #we need a log in form for our admin page
+
+class AddDeleteForm(Form) :
+    addDelete = RadioField("Are you adding or deleting", choices=[('Add', "Add"), ('Delete',  "Delete")], default="Add", id='add_delete')
+
 class LoginForm(Form):
     #the fields of our form
     username = StringField('Username', [validators.DataRequired()])
     password = PasswordField('Password', [validators.DataRequired()])
     submit = SubmitField('Log In')
-
-    #we need a little more validation since we are logging in
-    def validate(self):
-
-        #make sure base level validation is clear
-        initial_validation = super(LoginForm, self).validate()
-        if not initial_validation:
-            return False
-        
-       
-
-        #validation successful
-        return True
     
 class DesignForm(Form) :
     design = StringField("Design Name", [validators.DataRequired()])
@@ -55,3 +46,36 @@ class OptionForm(Form) :
     option_name = StringField("Name of option (red, blue)", [validators.DataRequired()])
     cost_change = DecimalField("Price increase", places=2, validators=[validators.DataRequired()])
     submit = SubmitField("Submit")
+
+class DeleteDesign(Form) :
+    design_id = SelectField('Delete design', choices=[], validators=[validators.DataRequired()])
+    submit = SubmitField('Delete')
+
+class DeleteType(Form) :
+    design_id = SelectField('Narrow down by design', choices=[])
+    type_id = SelectField('Delete type', choices=[], validators=[validators.DataRequired()])
+    submit = SubmitField('Delete')
+
+class DeleteProduct(Form) :
+    design_id = SelectField('Narrow down by design', choices=[])
+    type_id = SelectField('Narrow by type', choices=[])
+    product_id = SelectField('Delete product', choices=[], validators=[validators.DataRequired()])
+    submit = SubmitField('Delete')
+
+class DeleteCustom(Form) :
+    design_id = SelectField('Narrow down by design', choices=[])
+    type_id = SelectField('Narrow by type', choices=[])
+    product_id = SelectField('Narrow by product', choices=[])
+    custom_id = SelectField('Delete custom', choices=[], validators=[validators.DataRequired()])
+    submit = SubmitField('Delete')
+
+class DeleteOption(Form) :
+    design_id = SelectField('Narrow down by design', choices=[])
+    type_id = SelectField('Narrow down by type', choices=[])
+    product_id = SelectField('Narrow down by product', choices=[])
+    custom_id = SelectField('Narrow down by custom', choices=[])
+    option_id = SelectField('Delete option', choices=[], validators=[validators.DataRequired()])
+    submit = SubmitField('Delete')
+
+class addDeleteForm(Form) :
+    addDelete = RadioField('Add or Delete', choices=[('Add', 'Add'), ('Delete', 'Delete')], id='add_delete_field', default='Add')
