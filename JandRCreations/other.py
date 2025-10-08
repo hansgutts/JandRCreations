6ee9144d7344bc5e390e2e03fae30527f85080da
Hans Guttormsen <hansgutts@gmail.com>
2025-10-08 15:14:44 -0500

    page routes directly to type view if only one type

--- JandRCreations/other.py
+++ JandRCreations/other.py
@@ -1,10 +1,11 @@
 #this is a file for the misc functions and views that it doesn't make sense to dedicate file structure to
 import flask
+from flask import *
 
 from JandRCreations.auth import get_types_by_designid
 from JandRCreations.auth import get_all_designs
 from JandRCreations.auth import get_type_by_typeid
-
+from JandRCreations.auth import get_all_types
 bp = flask.Blueprint('other', __name__)
 
 @bp.route('/other/contact')
@@ -21,5 +22,9 @@ def index() :  #get every design and every type and send those through to the in
     #feels a little weird but stops me from making a million different functions to get info from db
     for design in designs : #go through all our designs and set the (key, value) pair as (design name, list of types with design name)
         designdict[design['prod_design']] = [get_type_by_typeid(id) for id in get_types_by_designid(design['prod_design_id'])]
+
+    types = get_all_types()
+    if len(types) == 1 :
+        return redirect(url_for('products.view_type', type = types[0]['prod_type_id']))
  
-    return flask.render_template('other/index.html', designdict=designdict) #render home page and send it the designs and types
+    return render_template('other/index.html', designdict=designdict) #render home page and send it the designs and types
